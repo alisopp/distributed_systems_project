12eae35dec70791a298de6cdca2bceba48945aeb
IIIlllllIlIllIllI <rtosir@outlook.com>
2018-06-18 16:21:33 +0200

    basic window

--- p2p_client/user_interface/src/com/distributed_systems/group_2/Controller.java
+++ p2p_client/user_interface/src/com/distributed_systems/group_2/Controller.java
@@ -1,4 +1,14 @@
 package com.distributed_systems.group_2;
 
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
 public class Controller {
+    @FXML
+    public void updateStage() throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("chat.fxml"));
+        Main.primaryStage.getScene().setRoot(root);
+    }
 }
--- p2p_client/user_interface/src/com/distributed_systems/group_2/Main.java
+++ p2p_client/user_interface/src/com/distributed_systems/group_2/Main.java
@@ -1,15 +1,18 @@
 package com.distributed_systems.group_2;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        this.primaryStage=primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
@@ -20,4 +23,7 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+
+
 }
